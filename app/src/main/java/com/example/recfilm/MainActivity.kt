a19a8fc344fc949cb8eeaacb8dcccebb786c4246
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-10-09 21:52:31 +0500

    Сделал Toast, анг. перевод меню.

--- app/src/main/java/com/example/recfilm/MainActivity.kt
+++ app/src/main/java/com/example/recfilm/MainActivity.kt
@@ -3,12 +3,31 @@ package com.example.recfilm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initButtons ()
+    }
 
+    private fun initButtons (){
+        button1.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
