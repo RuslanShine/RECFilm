04b7b2a40e0b022dd49eae7595c8f60f7ebf7d48
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-10-16 14:16:39 +0500

    1.Добавил иконки и меню для тулбара и навигации. 2.Удалил ненужные кнопки, сделал общий фон. 3.Откоректировал размеры банеров. 4.Добавил в ресурсы названия кнопок кнопок.

--- app/src/main/java/com/example/recfilm/MainActivity.kt
+++ app/src/main/java/com/example/recfilm/MainActivity.kt
@@ -10,24 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initButtons ()
+
     }
 
-    private fun initButtons (){
-        button1.setOnClickListener {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
-        }
-        button2.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-        }
-        button3.setOnClickListener {
-            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        button4.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        button5.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
-    }
+
 }
