20ac672f454df4b3aa7fec453f6876802af69a6b
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-10-16 20:35:07 +0500

    1.Добавил слушателя к кнопкам. 2.Добавил в ресурсы размеры (dimen) 3.обавил текст кнопок на английский. 4.Добавил тёмную тему.

--- app/src/main/java/com/example/recfilm/MainActivity.kt
+++ app/src/main/java/com/example/recfilm/MainActivity.kt
@@ -10,8 +10,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        onCreate()
+        initNavigation()
     }
 
+    fun onCreate() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun initNavigation() {
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
 }
