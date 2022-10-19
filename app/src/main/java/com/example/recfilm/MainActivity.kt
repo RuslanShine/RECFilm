8ab442f0b2508bca574e9dcebd600b6bc569e3ed
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-10-19 20:26:18 +0500

    Добавил R.string

--- app/src/main/java/com/example/recfilm/MainActivity.kt
+++ app/src/main/java/com/example/recfilm/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
-                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.menu_settings_toast), Toast.LENGTH_SHORT).show()
                     true
                 }
                 else -> false
@@ -31,15 +31,15 @@ class MainActivity : AppCompatActivity() {
 
             when (it.itemId) {
                 R.id.favorites -> {
-                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.menu_favorites), Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.watch_later -> {
-                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.menu_watch_later), Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.selections -> {
-                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.menu_selections), Toast.LENGTH_SHORT).show()
                     true
                 }
                 else -> false
