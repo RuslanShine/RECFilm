73a337e9b08a5ec8d8c32483d0eeb47c8c7cc31c
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-10-31 21:49:46 +0500

    Работает! задиние 24,7

--- app/src/main/java/com/example/recfilm/FilmListRecyclerAdapter.kt
+++ app/src/main/java/com/example/recfilm/FilmListRecyclerAdapter.kt
@@ -1,41 +1,53 @@
 package com.example.recfilm
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.film_item.view.*
 
-class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
+//в параметр передаем слушатель, чтобы мы потом могли обрабатывать нажатия из класса активити
+class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    //Здесь у нас хранится список элементов для RV
     private val items = mutableListOf<Film>()
 
+    //Этот метод нужно переопределить на возврат количества елементов в списке RV
+    override fun getItemCount() = items.size
+
+    //В этом методе мы привязываем наш view holder и передаем туда "надутую" верстку нашего фильма
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return FilmViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.film_item, parent, false)
-        )
+        return FilmViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.film_item, parent, false))
     }
 
+    //В этом методе будет привзяка полей из объекта Film, к view из film_item.xml
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        //Проверяем какой у нас ViewHolder
         when (holder) {
             is FilmViewHolder -> {
+                //Вызываем метод bind(), который мы создали и передаем туда объект
+                //из нашей базы данных с указанием позиции
                 holder.bind(items[position])
+                //Обрабатываем нажатие на весь элемент целиком(можно сделать на отдельный элемент
+                //напрмер, картинку) и вызываем метод нашего листенера, который мы получаем из
+                //конструктора адаптера
+                holder.itemView.item_container.setOnClickListener {
+                    clickListener.click(items[position])
+                }
             }
         }
     }
 
-    override fun getItemCount() = items.size
-
-    @SuppressLint("NotifyDataSetChanged")
+    //Метод для добавления объектов в наш список
     fun addItems(list: List<Film>) {
+        //Сначала очишаем(если не реализовать DiffUtils)
         items.clear()
+        //Добавляем
         items.addAll(list)
+        //Уведомляем RV, что пришел новый список и ему нужно заново все "привязывать"
         notifyDataSetChanged()
     }
 
+    //Интерфейс для обработки кликов
     interface OnItemClickListener {
         fun click(film: Film)
     }
-
-
 }
