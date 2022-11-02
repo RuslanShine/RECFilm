dc32c72ef72090355655c8a07995e7254035ec03
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-11-02 21:53:20 +0500

    DetailsFragment.kt, fragment_details.xml, запускаем фрагменты, меняем логику

---
+++ app/src/main/java/com/example/recfilm/DetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.recfilm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_details.*
+
+class DetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+        //Получаем наш фильм из переданного бандла
+        val film = arguments?.get("film") as Film
+
+        //Устанавливаем заголовок
+        details_toolbar.title = film.title
+        //Устанавливаем картинку
+        details_poster.setImageResource(film.poster)
+        //Устанавливаем описание
+        details_description.text = film.description
+    }
+}
+
