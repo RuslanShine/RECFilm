1f940c68b7546f8d47e97ba95fc01c1fbb7c8604
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-12-07 18:50:25 +0500

    Добавил dimen, const

--- app/src/main/java/com/example/recfilm/AnimationHelper.kt
+++ app/src/main/java/com/example/recfilm/AnimationHelper.kt
@@ -10,14 +10,14 @@ import kotlin.math.roundToInt
 
 object AnimationHelper {
     //расхождение круга от иконки
-    private const val menuItems = 4
+    private const val MENU_ITEMS = 4
     fun performFragmentCircularRevealAnimation(rootView: View, activity: Activity, position: Int) {
         //Новый тред
         Executors.newSingleThreadExecutor().execute {
             while (true) {
                 if (rootView.isAttachedToWindow) {
                     activity.runOnUiThread {
-                        val itemCenter = rootView.width / (menuItems * 2)
+                        val itemCenter = rootView.width / (MENU_ITEMS * 2)
                         val step = (itemCenter * 2) * (position - 1) + itemCenter
 
                         val x: Int = step
