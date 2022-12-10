b86bcac60a9d88751c83f81e9bd8a3ad4d13e5bb
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-12-10 11:57:07 +0500

    Test SearchView

--- app/src/androidTest/java/com/example/recfilm/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/recfilm/MainActivityTest.kt
@@ -1,17 +1,25 @@
 package com.example.recfilm
 
+
+import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 
 @RunWith(AndroidJUnit4::class)
@@ -24,4 +32,31 @@ val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
         onView(withId(R.id.main_recycler)).check(matches(isDisplayed()))
         onView(withId(R.id.main_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<FilmViewHolder>(0,click()))
     }
+
+
+
+    @Test
+    fun searchViewShouldBeAbleToInputText(){
+        val testString = "1111111"
+        onView(withId(R.id.search_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.search_view)).perform(typeSearchViewText(testString))
+    }
+
+    private fun typeSearchViewText(text: String?): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                //Ensure that only apply if it is a SearchView and if it is visible.
+                return allOf(isDisplayed(), isAssignableFrom(SearchView::class.java))
+            }
+
+            override fun getDescription(): String {
+                return "Change view text"
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                (view as SearchView).setQuery(text, false)
+            }
+        }
+    }
+
 }
