de51e0425cb0ae7715ef07e6aea11ed35392ef33
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-12-10 11:42:02 +0500

    Test RV

---
+++ app/src/androidTest/java/com/example/recfilm/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package com.example.recfilm
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+@get:Rule
+val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun recyclerViewShouldBeAttached(){
+        onView(withId(R.id.main_recycler)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<FilmViewHolder>(0,click()))
+    }
+}
