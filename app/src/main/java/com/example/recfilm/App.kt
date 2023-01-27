dc9b606421bdf918d562e6002a3c874a2c8eb248
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-01-27 19:25:51 +0500

    Koin

--- app/src/main/java/com/example/recfilm/App.kt
+++ app/src/main/java/com/example/recfilm/App.kt
@@ -5,51 +5,25 @@ import android.app.Application
 import com.example.recfilm.data.ApiConstants
 import com.example.recfilm.data.MainRepository
 import com.example.recfilm.data.TmdbApi
+import com.example.recfilm.di.DI
 import com.example.recfilm.domain.Interactor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class App : Application() {
-    lateinit var repo: MainRepository
-    lateinit var interactor: Interactor
-    private val CALL_TIMEOUT: Long = 30
-    private val READ_TIMOUT: Long = 30
 
     override fun onCreate() {
         super.onCreate()
-        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
-        instance = this
-        //Инициализируем репозиторий
-        repo = MainRepository()
-        //Создаём кастомный киент
-        val okHttpClient = OkHttpClient.Builder()
-            .callTimeout(CALL_TIMEOUT, TimeUnit.SECONDS)
-            .readTimeout(READ_TIMOUT, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
-        //Создаём Ретрофит
-        val retrofit = Retrofit.Builder()
-            .baseUrl(ApiConstants.BASE_USL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-        //Создаем сам сервис с методами для запросов
-        val retrofitService = retrofit.create(TmdbApi::class.java)
-        //Инициализируем интерактор
-        interactor = Interactor(repo, retrofitService)
-    }
-
-    companion object {
-        //Здесь статически хранится ссылка на экземпляр App
-        lateinit var instance: App
-            //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
-            private set
+        startKoin {
+            androidContext(this@App)
+            androidLogger()
+            modules(listOf(DI.mainModule))
+        }
     }
 }
--- app/src/main/java/com/example/recfilm/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/recfilm/viewmodel/HomeFragmentViewModel.kt
@@ -3,19 +3,23 @@ package com.example.recfilm.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.recfilm.App
 import com.example.recfilm.domain.Film
 import com.example.recfilm.domain.Interactor
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
+
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
     private val _filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
     val filmsListLiveData: LiveData<List<Film>>
         get() = _filmsListLiveData
-    private var interactor: Interactor = App.instance.interactor
+
+    //Инициализируем интерактор
+    private val interactor: Interactor by inject()
 
     init {
-        val films = interactor.getFilmsFromApi(1, object : ApiCallback{
-            override fun onSuccess(films: List<Film>){
+        interactor.getFilmsFromApi(1, object : ApiCallback {
+            override fun onSuccess(films: List<Film>) {
                 _filmsListLiveData.postValue(films)
             }
 
