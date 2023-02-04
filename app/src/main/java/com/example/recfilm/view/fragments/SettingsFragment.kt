97d4e9892f586bfd093168950449ae0bb0eea3ed
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-02-04 21:35:04 +0500

    SettingsFragment

--- app/src/main/java/com/example/recfilm/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/recfilm/view/fragments/SettingsFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.recfilm.R
 import com.example.recfilm.databinding.FragmentSettingsBinding
+import com.example.recfilm.utils.AnimationHelper
 import com.example.recfilm.viewmodel.SettingsFragmentViewModel
+import kotlinx.android.synthetic.main.fragment_selections.*
 
 
 class SettingsFragment : Fragment() {
@@ -29,6 +32,27 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        AnimationHelper.performFragmentCircularRevealAnimation(selections_fragment_root,
+            requireActivity(),
+            5)
+        viewModel.categoryPropertyLifeData.observe(viewLifecycleOwner, Observer<String>
+        {
+            when (it) {
+                POPULAR_CATEGORY -> binding.radioGroup.check(R.id.radio_popular)
+                TOP_RATED_CATEGORY -> binding.radioGroup.check(R.id.radio_top_rated)
+                UPCOMING_CATEGORY -> binding.radioGroup.check(R.id.radio_upcoming)
+                NOW_PLAYING_CATEGORY -> binding.radioGroup.check(R.id.radio_now_playing)
+            }
+        })
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radio_popular -> viewModel.putCategoryProperty(POPULAR_CATEGORY)
+                R.id.radio_top_rated -> viewModel.putCategoryProperty(TOP_RATED_CATEGORY)
+                R.id.radio_upcoming -> viewModel.putCategoryProperty(UPCOMING_CATEGORY)
+                R.id.radio_now_playing -> viewModel.putCategoryProperty(NOW_PLAYING_CATEGORY)
+            }
+        }
     }
 
     companion object {
