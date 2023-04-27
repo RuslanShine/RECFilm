f62fb4be13e6530ba80e1a37ebf55d849f446c9d
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-04-27 19:12:52 +0500

    dimen нотификации

--- app/src/main/java/com/example/recfilm/view/notofications/NotificationHelper.kt
+++ app/src/main/java/com/example/recfilm/view/notofications/NotificationHelper.kt
@@ -1,6 +1,5 @@
 package com.example.recfilm.view.notofications
 
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
