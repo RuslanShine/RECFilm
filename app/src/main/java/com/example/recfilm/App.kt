4b56e201e562a93f46bdf6d008679f26de9b01d6
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2023-04-27 18:56:05 +0500

    Нотификация "Не забудьте осмотреть фильм"

--- app/src/main/java/com/example/recfilm/App.kt
+++ app/src/main/java/com/example/recfilm/App.kt
@@ -1,10 +1,14 @@
 package com.example.recfilm
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.example.recfilm.di.AppComponent
 import com.example.recfilm.di.DaggerAppComponent
 import com.example.recfilm.di.modules.DatabaseModule
 import com.example.recfilm.di.modules.DomainModule
+import com.example.recfilm.view.notofications.NotificationConstants.CHANNEL_ID
 import com.example.remote_module.DaggerRemoteComponent
 
 class App : Application() {
@@ -20,6 +24,21 @@ class App : Application() {
             .databaseModule(DatabaseModule())
             .domainModule(DomainModule(this))
             .build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Задаем имя, описание и важность канала
+            val name = "WatchLaterChannel"
+            val descriptionText = "RecFilms notification Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            //Создаем канал, передав в параметры его ID(строка), имя(строка), важность(константа)
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            //Отдельно задаем описание
+            mChannel.description = descriptionText
+            //Получаем доступ к менеджеру нотификаций
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            //Регистрируем канал
+            notificationManager.createNotificationChannel(mChannel)
+        }
     }
 
     companion object {
